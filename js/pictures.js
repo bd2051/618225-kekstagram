b52166c16bb2d114749978cb450658138fce80f1
Дмитрий Быков <bd2051@mail.ru>
2018-06-16 16:36:32 +0300

    добавлена отрисовка фотографий

--- js/pictures.js
+++ js/pictures.js
@@ -25,7 +25,7 @@ var createPhotoGallery = function () {
   var PHOTOS_NUMBER = 25;
   var MAX_LIKES = 200;
   var MIN_LIKES = 15;
-  var randomComments = [
+  var comments = [
     'Всё отлично!',
     'В целом всё неплохо. Но не всё.',
     'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -33,7 +33,7 @@ var createPhotoGallery = function () {
     'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
     'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
   ];
-  var randomDescription = [
+  var description = [
     'Тестим новую камеру!',
     'Затусили с друзьями на море',
     'Как же круто тут кормят',
@@ -41,11 +41,37 @@ var createPhotoGallery = function () {
     'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
     'Вот это тачка!',
   ];
-  var photos = [];
+  var photoGallery = [];
   for (var i = 0; i < PHOTOS_NUMBER; i++) {
-    photos[i] = createRandomPhoto(i + 1, MAX_LIKES, MIN_LIKES, randomComments, randomDescription);
+    photoGallery[i] = createRandomPhoto(i + 1, MAX_LIKES, MIN_LIKES, comments, description);
   }
-  return photos;
-}
+  return photoGallery;
+};
 
-var photoGallery = createPhotoGallery();
+var photos = createPhotoGallery();
+var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture__link');
+
+var drawPhoto = function (photo) {
+  var pictureElement = pictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').src = photo.url;
+  pictureElement.querySelector('.picture__stat--likes').textContent = photo.likes;
+  pictureElement.querySelector('.picture__stat--comments').textContent = photo.comments;
+
+  return pictureElement;
+};
+
+var pictures = document.querySelector('.pictures');
+
+var fillPhotoGallery = function (photoGallery) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < photoGallery.length; i++) {
+    fragment.appendChild(drawPhoto(photoGallery[i]));
+  }
+  pictures.appendChild(fragment);
+};
+
+fillPhotoGallery(photos);
+
+var bigPicture = document.querySelector('.big-picture');
+bigPicture.classList.remove('hidden');
