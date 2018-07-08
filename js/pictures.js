0767cabd9e67a558af01c6a251298ca386175143
Дмитрий Быков <bd2051@mail.ru>
2018-07-08 15:18:18 +0300

    инкапсуляция

--- js/pictures.js
+++ js/pictures.js
@@ -1,145 +1,32 @@
 'use strict';
 
-var generateNaturalNumber = function (max, min) {
-  return Math.floor(Math.random() * (max - min) + min);
-};
+(function () {
+  var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture__link');
 
-var createRandomPhoto = function (photoNumber, maxLikes, minLikes, photoComments, photoDescription) {
-  var createRandomComment = function (comments) {
-    var maxNumber = comments.length - 1;
-    var minNumber = 0;
-    var randomNumber = generateNaturalNumber(maxNumber, minNumber);
-    var comment = [];
-    comment[0] = comments[randomNumber];
-    if (Math.round(Math.random())) {
-      minNumber = 1;
-      randomNumber += generateNaturalNumber(maxNumber, minNumber);
-      randomNumber -= (randomNumber > maxNumber) ? comments.length : 0;
-      comment[1] = comments[randomNumber];
-    }
-    return comment;
+  var drawPhoto = function (photo) {
+    var pictureElement = pictureTemplate.cloneNode(true);
+
+    pictureElement.querySelector('.picture__img').src = photo.url;
+    pictureElement.querySelector('.picture__stat--likes').textContent = photo.likes;
+    pictureElement.querySelector('.picture__stat--comments').textContent = photo.comments.length;
+
+    return pictureElement;
   };
-  return {
-    url: 'photos/' + photoNumber + '.jpg',
-    likes: generateNaturalNumber(maxLikes, minLikes),
-    comments: createRandomComment(photoComments),
-    description: photoDescription[generateNaturalNumber(photoDescription.length - 1, 0)]
-  };
-};
 
-var createPhotoGallery = function () {
-  var PHOTOS_NUMBER = 25;
-  var MAX_LIKES = 200;
-  var MIN_LIKES = 15;
-  var comments = [
-    'Всё отлично!',
-    'В целом всё неплохо. Но не всё.',
-    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
-    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
-  ];
-  var description = [
-    'Тестим новую камеру!',
-    'Затусили с друзьями на море',
-    'Как же круто тут кормят',
-    'Отдыхаем...',
-    'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
-    'Вот это тачка!',
-  ];
-  var photoGallery = [];
-  for (var i = 0; i < PHOTOS_NUMBER; i++) {
-    photoGallery[i] = createRandomPhoto(i + 1, MAX_LIKES, MIN_LIKES, comments, description);
-  }
-  return photoGallery;
-};
+  var pictures = document.querySelector('.pictures');
+  var picturesElement = [];
 
-var photos = createPhotoGallery();
-var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture__link');
-
-var drawPhoto = function (photo) {
-  var pictureElement = pictureTemplate.cloneNode(true);
-
-  pictureElement.querySelector('.picture__img').src = photo.url;
-  pictureElement.querySelector('.picture__stat--likes').textContent = photo.likes;
-  pictureElement.querySelector('.picture__stat--comments').textContent = photo.comments.length;
-
-  return pictureElement;
-};
-
-var pictures = document.querySelector('.pictures');
-var picturesElement = [];
-
-var fillPhotoGallery = function (photoGallery) {
-  var fragment = document.createDocumentFragment();
-  for (var i = 0; i < photoGallery.length; i++) {
-    fragment.appendChild(drawPhoto(photoGallery[i]));
-  }
-  pictures.appendChild(fragment);
-  picturesElement = pictures.querySelectorAll('.picture__link');
-};
-
-fillPhotoGallery(photos);
-
-var bigPicture = document.querySelector('.big-picture');
-
-var drawBigPhoto = function (photo) {
-  bigPicture.querySelector('.big-picture__img img').src = photo.url;
-  bigPicture.querySelector('.likes-count').textContent = photo.likes;
-  var socialComment = bigPicture.querySelectorAll('.social__comment');
-  var MAX_AVATAR_NUMBER = 6;
-  var MIN_AVATAR_NUMBER = 1;
-  for (var i = 0; i < socialComment.length; i++) {
-    if (photo.comments[i]) {
-      var avatarSource = 'img/avatar-' + generateNaturalNumber(MAX_AVATAR_NUMBER, MIN_AVATAR_NUMBER) + '.svg';
-      socialComment[i].querySelector('.social__picture').src = avatarSource;
-      socialComment[i].querySelector('.social__text').textContent = photo.comments[i];
-    } else {
-      socialComment[i].classList.add('visually-hidden');
+  var fillPhotoGallery = function (photoGallery) {
+    var fragment = document.createDocumentFragment();
+    for (var i = 0; i < photoGallery.length; i++) {
+      fragment.appendChild(drawPhoto(photoGallery[i]));
     }
-  }
-  bigPicture.querySelector('.social__caption').textContent = photo.description;
-};
+    pictures.appendChild(fragment);
+    picturesElement = pictures.querySelectorAll('.picture__link');
+  };
 
-var hideBlock = function (blockParent, blockClass) {
-  blockParent.querySelector(blockClass).classList.add('visually-hidden');
-};
-
-var bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
-
-var onBigPictureCloseClick = function () {
-  closeBigPhoto();
-};
-
-var ESC_KEYCODE = 27;
-
-var onBigPictureEscPress = function (evt) {
-  if (evt.keyCode === ESC_KEYCODE) {
-    closeBigPhoto();
-  }
-};
-
-var openBigPhoto = function (photo) {
-  bigPicture.classList.remove('hidden');
-  drawBigPhoto(photo);
-  hideBlock(bigPicture, '.social__comment-count');
-  hideBlock(bigPicture, '.social__loadmore');
-  bigPictureClose.addEventListener('click', onBigPictureCloseClick);
-  document.addEventListener('keydown', onBigPictureEscPress);
-};
-
-var closeBigPhoto = function () {
-  bigPicture.classList.add('hidden');
-  bigPictureClose.removeEventListener('click', onBigPictureCloseClick);
-  document.removeEventListener('keydown', onBigPictureEscPress);
-};
-
-var addOpeningBigPhoto = function (index) {
-  picturesElement[index].addEventListener('click', function () {
-    openBigPhoto(photos[index]);
-  });
-};
-
-for (var i = 0; i < picturesElement.length; i++) {
-  addOpeningBigPhoto(i);
-}
+  fillPhotoGallery(window.data.photos);
+  window.pictures = {
+    element: picturesElement
+  };
+})();
---
+++ js/preview.js
@@ -0,0 +1,58 @@
+'use strict';
+
+(function () {
+  var bigPicture = document.querySelector('.big-picture');
+
+  var drawBigPhoto = function (photo) {
+    bigPicture.querySelector('.big-picture__img img').src = photo.url;
+    bigPicture.querySelector('.likes-count').textContent = photo.likes;
+    var socialComment = bigPicture.querySelectorAll('.social__comment');
+    var MAX_AVATAR_NUMBER = 6;
+    var MIN_AVATAR_NUMBER = 1;
+    for (var i = 0; i < socialComment.length; i++) {
+      if (photo.comments[i]) {
+        var avatarSource = 'img/avatar-' + window.util.generateNaturalNumber(MAX_AVATAR_NUMBER, MIN_AVATAR_NUMBER) + '.svg';
+        socialComment[i].querySelector('.social__picture').src = avatarSource;
+        socialComment[i].querySelector('.social__text').textContent = photo.comments[i];
+      } else {
+        socialComment[i].classList.add('visually-hidden');
+      }
+    }
+    bigPicture.querySelector('.social__caption').textContent = photo.description;
+  };
+
+  var bigPictureClose = bigPicture.querySelector('.big-picture__cancel');
+
+  var onBigPictureCloseClick = function () {
+    closeBigPhoto();
+  };
+
+  var onBigPictureEscPress = function (evt) {
+    window.util.isEscEvent(evt, closeBigPhoto());
+  };
+
+  var openBigPhoto = function (photo) {
+    bigPicture.classList.remove('hidden');
+    drawBigPhoto(photo);
+    window.util.hideBlock(bigPicture, '.social__comment-count');
+    window.util.hideBlock(bigPicture, '.social__loadmore');
+    bigPictureClose.addEventListener('click', onBigPictureCloseClick);
+    document.addEventListener('keydown', onBigPictureEscPress);
+  };
+
+  var closeBigPhoto = function () {
+    bigPicture.classList.add('hidden');
+    bigPictureClose.removeEventListener('click', onBigPictureCloseClick);
+    document.removeEventListener('keydown', onBigPictureEscPress);
+  };
+
+  var addOpeningBigPhoto = function (index) {
+    window.pictures.element[index].addEventListener('click', function () {
+      openBigPhoto(window.data.photos[index]);
+    });
+  };
+
+  for (var i = 0; i < window.pictures.element.length; i++) {
+    addOpeningBigPhoto(i);
+  }
+})();
